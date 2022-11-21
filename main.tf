8bb935897a230983906f6ae0cec3fb4514307adf
John Matthew <john@compunique.com>
2022-11-21 08:16:58 -0800

    subnets and security groups

--- main.tf
+++ main.tf
@@ -31,5 +31,47 @@ resource "azurerm_virtual_network" "mtc-vn" {
     environment = "dev"
   }
 
+}
 
+resource "azurerm_subnet" "mtc-subnet" {
+  name = "mtc-subnet"
+
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+
+  address_prefixes = [
+    "10.123.1.0/24"
+  ]
+
+}
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name = "mtc-sg"
+
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+
+}
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
 }
